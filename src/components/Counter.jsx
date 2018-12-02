4238c5cf6964ba5eae0179ca8325f58f4213977a
jonroby <joncaroby@gmail.com>
2018-12-01 18:01:44 -0800

    js to jsx and fix all eslint errors

---
+++ src/components/Counter.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Counter from "./Counter";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          {/* <Navigation> */}
+          <Switch>
+            <Route path="/" component={Counter} exact />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default App;
